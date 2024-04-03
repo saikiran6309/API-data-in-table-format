94b903fc525424a4e2b9b264323c5dc7c28542ca
sai kiran reddy <saikiranreddynaredla@gmail.com>
2024-04-03 11:25:36 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
+
+  const [result,setresult]=useState([]);
+
+  useEffect(() => {
+
+    fetch(`https://data.covid19india.org/data.json`).then(
+      Response=>Response.json()
+    ).then(data => setresult(data.statewise))
+
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <center>
+    <h3>INDIA COVID 19 RECORD ACCORDING TO THE STATES</h3>
+    <table className="table table-bordered border-primary">
+      <thead>
+        <tr>
+          <th className='th dark'>state</th>
+          <th>active</th>
+          <th>confirmed</th>
+          <th>deaths</th>
+          <th>recovered</th>
+          <th>lastupdatedtime</th>
+
+        </tr>
+      </thead>
+      <tbody>
+     
+          {
+            result.map(item =>{
+              return(
+                <tr>
+                  <td>{item.state}</td>
+                  <td>{item.active}</td>
+                  <td>{item.confirmed}</td>
+                  <td>{item.deaths}</td>
+                  <td>{item.recovered}</td>
+                  <td>{item.lastupdatedtime}</td>
+                  
+                </tr>
+              )
+            })
+          }
+       
+
+      </tbody>
+    </table>
+    
+
+    </center>
+   </div>
   );
 }
 
